6cd25887b7d121d96f02f2bf14ec8bb83af2d9cb
Juan Sanz <juan.sanz@opendeusto.es>
2022-03-29 18:53:36 +0200

    Ventana de habitaciones sin funcion

---
+++ HotelUD/src/main/java/es/deusto/spq/RoomWindow.java
@@ -0,0 +1,161 @@
+package main.java.es.deusto.spq;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.UIManager;
+import javax.swing.JSplitPane;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+
+public class RoomWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RoomWindow frame = new RoomWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RoomWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 539, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("HABITACIONES");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JButton btnNewButton = new JButton("BACK");
+		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, UIManager.getColor("List.selectionBackground"), null, null, null));
+		panel_1.setBounds(251, 11, 252, 125);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 252, 125);
+		panel_1.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Room Number", "Type", "Floor", "Surface", "Price"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Integer.class, String.class, Integer.class, Integer.class, Double.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		scrollPane.setViewportView(table);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, UIManager.getColor("List.selectionBackground"), null, null, null));
+		panel_2.setBounds(0, 0, 229, 203);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Room number");
+		lblNewLabel_1.setBounds(10, 11, 119, 14);
+		panel_2.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Type");
+		lblNewLabel_2.setBounds(10, 36, 46, 14);
+		panel_2.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Floor");
+		lblNewLabel_3.setBounds(10, 61, 46, 14);
+		panel_2.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Surface");
+		lblNewLabel_4.setBounds(10, 86, 46, 14);
+		panel_2.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Price");
+		lblNewLabel_5.setBounds(10, 111, 46, 14);
+		panel_2.add(lblNewLabel_5);
+		
+		textField = new JTextField();
+		textField.setBounds(139, 8, 80, 20);
+		panel_2.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(139, 33, 80, 20);
+		panel_2.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(139, 58, 80, 20);
+		panel_2.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(139, 83, 80, 20);
+		panel_2.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(139, 108, 80, 20);
+		panel_2.add(textField_4);
+		
+		JButton btnNewButton_1 = new JButton("ADD");
+		btnNewButton_1.setBounds(74, 154, 89, 23);
+		panel_2.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("ASSIGN");
+		btnNewButton_2.setBounds(261, 155, 89, 23);
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("DELETE");
+		btnNewButton_3.setBounds(414, 155, 89, 23);
+		panel.add(btnNewButton_3);
+	}
+}
