4073e5b8ab9d2d9586f48e3acc0c8c10e5096d51
Diego <diegoburzaco@opendeusto.es>
2022-03-31 22:56:22 +0200

    tests de las clases

--- HotelUD/src/classes/Room.java
+++ HotelUD/src/classes/Room.java
@@ -1,9 +1,11 @@
 package classes;
+
 public class Room {
-    private Integer numberDoor; 
-    private String type;
-    private int surface;
-    private int floor;
+	private Integer numberDoor;
+	private String type;
+	private int surface;
+	private int floor;
+
 	public Room(Integer numberDoor, String type, int surface, int floor) {
 		super();
 		this.numberDoor = numberDoor;
@@ -11,6 +13,7 @@ public class Room {
 		this.surface = surface;
 		this.floor = floor;
 	}
+
 	public Room() {
 		super();
 		this.numberDoor = 0;
@@ -18,33 +21,42 @@ public class Room {
 		this.surface = 0;
 		this.floor = 0;
 	}
+
 	public Integer getNumberDoor() {
 		return numberDoor;
 	}
+
 	public void setNumberDoor(Integer numberDoor) {
 		this.numberDoor = numberDoor;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public int getSurface() {
 		return surface;
 	}
+
 	public void setSurface(int surface) {
 		this.surface = surface;
 	}
+
 	public int getFloor() {
 		return floor;
 	}
+
 	public void setFloor(int floor) {
 		this.floor = floor;
 	}
+
 	@Override
 	public String toString() {
 		return "Room numberDoor: " + numberDoor + ", type: " + type + ", surface: " + surface + ", floor: " + floor;
 	}
-	
+
 }
--- HotelUD/src/classes/User.java
+++ HotelUD/src/classes/User.java
@@ -1,13 +1,15 @@
 package classes;
+
 public class User {
 	private int counter = 0;
-	private String name; 
-    private String dni; 
-    private String gender;
-    private int age;
+	private String name;
+	private String dni;
+	private String gender;
+	private int age;
 	private String surname;
 	private String bankCount;
-	public User(int counter,String name, String dni, String gender, int age, String surname, String bankCount) {
+
+	public User(int counter, String name, String dni, String gender, int age, String surname, String bankCount) {
 		super();
 		this.counter = counter++;
 		this.name = name;
@@ -17,6 +19,7 @@ public class User {
 		this.surname = surname;
 		this.bankCount = bankCount;
 	}
+
 	public User() {
 		super();
 		this.counter = counter++;
@@ -27,46 +30,59 @@ public class User {
 		this.surname = "";
 		this.bankCount = "";
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDni() {
 		return dni;
 	}
+
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getSurname() {
 		return surname;
 	}
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
+
 	public String getBankCount() {
 		return bankCount;
 	}
+
 	public void setBankCount(String bankCount) {
 		this.bankCount = bankCount;
 	}
+
 	@Override
 	public String toString() {
-		return "User code "+counter+" name: " + name + ", dni: " + dni + ", gender: " + gender + ", age: " + age + ", surname: " + surname
-				+ ", bankCount: " + bankCount;
+		return "User code " + counter + " name: " + name + ", dni: " + dni + ", gender: " + gender + ", age: " + age
+				+ ", surname: " + surname + ", bankCount: " + bankCount;
 	}
 
 }
