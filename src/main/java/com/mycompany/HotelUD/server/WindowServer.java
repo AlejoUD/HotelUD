092e31ebe270efee9379dc83979d1f3e2776ea9e
AnderMoldes <ander.moldes@opendeusto.es>
2022-04-28 18:35:36 +0200

    Arreglo de la ventana del servidor

--- src/main/java/com/mycompany/HotelUD/server/WindowServer.java
+++ src/main/java/com/mycompany/HotelUD/server/WindowServer.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.border.EmptyBorder;
 
 import com.mycompany.HotelUD.BBDD.BBDD;
@@ -13,14 +14,19 @@ import com.mycompany.HotelUD.classes.User;
 import com.mycompany.HotelUD.classes.Worker;
 
 import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.JList;
 import javax.swing.UIManager;
 import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.Action;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JMenuBar;
@@ -53,49 +59,119 @@ public class WindowServer extends JFrame {
 	public WindowServer() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 764, 531);
+		 this.setTitle("Server Home");
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenuItem ItemHome = new JMenuItem("Home");
-		menuBar.add(ItemHome);
+		JMenu menu1= new JMenu("Menu");
+		menuBar.add(menu1);
+		
+		JMenuItem menu2= new JMenuItem("Exit");
+		menuBar.add(menu2);
+		
+		menu2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		
 		
 		JMenuItem ItemWorkers = new JMenuItem("Workers");
-		menuBar.add(ItemWorkers);
+		menu1.add(ItemWorkers);
+		
+		ItemWorkers.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+			
+		});
 		
 		JMenuItem ItemRooms = new JMenuItem("Rooms");
-		menuBar.add(ItemRooms);
+		menu1.add(ItemRooms);
+		
+		ItemRooms.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+			
+		});
 		
 		JMenuItem ItemClients = new JMenuItem("Clients");
-		menuBar.add(ItemClients);
+		menu1.add(ItemClients);
+		
+		ItemClients.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+			
+		});
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		
+		JButton exitButton = new JButton("EXIT");
+		exitButton.setBounds(649, 434, 89, 23);
+		contentPane.add(exitButton);
+		
+		exitButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+			
+		});
+		
+		
+		
+		//Rooms
+		
 		JPanel room_panel = new JPanel();
-		room_panel.setBackground(Color.DARK_GRAY);
-		room_panel.setBorder(UIManager.getBorder("TitledBorder.border"));
-		room_panel.setBounds(423, 42, 315, 181);
-		contentPane.add(room_panel);
 		room_panel.setLayout(null);
+		room_panel.setBorder(UIManager.getBorder("TitledBorder.border"));
+		room_panel.setBackground(Color.DARK_GRAY);
+		room_panel.setBounds(423, 42, 315, 181);
 		
-		JScrollPane room_scrollPane = new JScrollPane();
-//		room_scrollPane.setBounds(10, 11, 295, 159);
-		room_panel.add(room_scrollPane);
 		
-		DefaultListModel modelRoom = new DefaultListModel<>();
-//		Room room = new Room();
-//		System.out.println( baseDatos.getRooms());
-//		ArrayList<Room> arrayR = new ArrayList<>();
-//		for (Room r : baseDatos.getRooms()) {
-//			modelRoom.addElement(r);
-//		}
+		
+		DefaultListModel<Room> modelRoom = new DefaultListModel<>();
+		Room room = new Room();
+		System.out.println( baseDatos.getRooms());
+		ArrayList<Room> arrayR = new ArrayList<>();
+		for (Room r : baseDatos.getRooms()) {
+			modelRoom.addElement(r);
+		}
 		
 		JList room_list = new JList(modelRoom);
-		room_list.setBounds(0, 0, 1, 1);
+		room_list.setBounds(10, 11, 295, 159);
 		room_panel.add(room_list);
 		
+		JScrollPane room_scrollPane = new JScrollPane(room_list);
+//		room_scrollPane.setBounds(10, 11, 295, 159);
+		room_scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		room_panel.add(room_list);
+		contentPane.add(room_panel);
+		
+		
+		JLabel labelRooms = new JLabel("Rooms");
+		labelRooms.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 11));
+		labelRooms.setBounds(423, 17, 46, 14);
+		contentPane.add(labelRooms);
+		
+		
+		//Clients
 		JPanel client_panel = new JPanel();
 		client_panel.setLayout(null);
 		client_panel.setBorder(UIManager.getBorder("TitledBorder.border"));
@@ -104,7 +180,7 @@ public class WindowServer extends JFrame {
 		contentPane.add(client_panel);
 		
 		JScrollPane client_scrollPane = new JScrollPane();
-//		client_scrollPane.setBounds(10, 11, 295, 159);
+		//client_scrollPane.setBounds(10, 11, 295, 159);
 		client_panel.add(client_scrollPane);
 		
 		DefaultListModel modelUsers = new DefaultListModel<>();
@@ -114,26 +190,18 @@ public class WindowServer extends JFrame {
 		for (User u : baseDatos.getUsers()) {
 			modelUsers.addElement(u);
 		}
-	
 		
 		JList client_list = new JList(modelUsers);
-		client_list.setBounds(0, 0, 1, 1);
+		client_list.setBounds(10, 11, 295, 159);
 		client_panel.add(client_list);
-		
-		JButton exitButton = new JButton("EXIT");
-		exitButton.setBounds(649, 434, 89, 23);
-		contentPane.add(exitButton);
 		
 		JLabel labelClient = new JLabel("Clients");
 		labelClient.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 11));
 		labelClient.setBounds(10, 17, 46, 14);
 		contentPane.add(labelClient);
 		
-		JLabel labelRooms = new JLabel("Rooms");
-		labelRooms.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 11));
-		labelRooms.setBounds(423, 17, 46, 14);
-		contentPane.add(labelRooms);
 		
+		// Workers
 		JPanel worker_panel = new JPanel();
 		worker_panel.setLayout(null);
 		worker_panel.setBorder(UIManager.getBorder("TitledBorder.border"));
