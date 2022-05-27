70d1c8669a80e810225747a261f0c81240add54c
Juan Sanz Gallego <juan.sanz@opendeusto.es>
2022-05-27 13:17:35 +0200

    Performance test de Server iniciado

---
+++ src/test/java/com/mycompany/HotelUD/server/HotelServerTest.java
@@ -0,0 +1,53 @@
+package com.mycompany.HotelUD.server;
+
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.core.Response;
+
+import com.mycompany.HotelUD.classes.Worker;
+import com.mycompany.HotelUD.classes.User;
+
+@PerfTest(invocations = 5)
+@Required(max = 1200, average = 250)
+public class HotelServerTest {
+
+    private HotelCollector hotelCollector;
+    private Worker worker;
+    private User user;
+
+    @Rule public ContiPerfRule rule = new ContiPerfRule();
+   
+    @Before public void setUp(){
+        hotelCollector = new HotelCollector();
+        worker = new Worker(1, "Juan", "123456789", "Male", 21);
+        user = new User(1, "Diego", "987654321", "Male", 21, "123", "1234567890");
+    }
+
+	@Test
+	public void TestaddWorker() {
+		//Response expected = 
+        //assertEquals(expected, hotelCollector.addWorker(worker));
+	}
+
+	@Test
+	public void TestgetWorkers() {
+
+	}
+
+	@Test
+	public void TestaddUser(User user) {
+		hotelCollector.addUser(user);
+	}
+
+	@Test
+	public void TestgetUsers() {
+
+	}
+}
